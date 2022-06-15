4f8dbad2d189852b3287f41bf436dbb8c0aa7ba1
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-06-15 13:53:29 -0400

    Initial content

--- src/SortedIteratorProducts.jl
+++ src/SortedIteratorProducts.jl
@@ -1,5 +1,127 @@
 module SortedIteratorProducts
 
-# Write your package code here.
+using DataStructures: BinaryHeap
+
+export SortedIteratorProduct
+
+struct SortedIteratorProduct{T<:Tuple, F<:Function}
+    sources::T
+    by::F
+end
+
+"""
+    SortedIteratorProduct(by::Function, iterators...)
+
+Construct an iterator over the cartesian product of `iterators` that is sorted according to
+`by`. `by` must be non-decreasing over the indices of `iterators`.
+
+For example, if there are two iterators `a` and `b`, then
+`by(first(a), first(b)) ≤ by(first(a), second(b))` where `second(b)` is the second element
+of `b`.
+
+```jldoctest
+julia> using ..SortedIteratorProducts, Base.Iterators
+
+julia> A = Iterators.Count(1, 1)
+Base.Iterators.Count{Int64, Int64}(1, 1)
+
+julia> B = Iterators.Count(3, 2)
+Base.Iterators.Count{Int64, Int64}(3, 2)
+
+julia> x = SortedIteratorProduct(sum, A, B);
+
+julia> collect(take(x, 6))
+6-element Vector{Tuple{Int64, Int64}}:
+ (1, 3)
+ (2, 3)
+ (3, 3)
+ (1, 5)
+ (4, 3)
+ (2, 5)
+
+julia> x = SortedIteratorProduct(((b,a),) -> b^a, B, A);
+
+julia> collect(take(x, 6))
+6-element Vector{Tuple{Int64, Int64}}:
+ (3, 1)
+ (5, 1)
+ (7, 1)
+ (9, 1)
+ (3, 2)
+ (11, 1)
+```
+"""
+function SortedIteratorProduct(by::Function, iterators...)
+    sources = cached.(iterators)
+    SortedIteratorProduct(sources, by)
+end
+
+lookup(sip, x) = tuple((s[i] for (s, i) in zip(sip.sources, x))...)
+function Base.iterate(sip::SortedIteratorProduct)
+    all(x -> checkbounds(Bool, x, 1), sip.sources) || return nothing
+    one = map(_->1, sip.sources)
+    iterate(sip, (Set((one,)), BinaryHeap(Base.By(x -> (sip.by(lookup(sip, x)), reverse(x))), [one])))
+end
+function Base.iterate(sip::SortedIteratorProduct, (set, heap))
+    isempty(heap) && return nothing
+    indices = pop!(heap)
+
+    for i in eachindex(indices)
+        new = ntuple(j -> indices[j] + (j == i), length(indices))
+        if checkbounds(Bool, sip.sources[i], indices[i]+1) && new ∉ set
+            push!(set, new)
+            push!(heap, new)
+        end
+    end
+    lookup(sip, indices), (set, heap)
+end
+Base.collect(sip::SortedIteratorProduct) = sort!(vec(collect(Base.Iterators.product(sip.sources...))); by=sip.by)
+
+function multiply(i1::Base.IteratorSize, i2::Base.IteratorSize)
+    (i1, i2) isa NTuple{2, Base.IsInfinite} && return Base.IsInfinite()
+    (i1, i2) isa NTuple{2, Union{Base.HasLength, Base.HasShape}} && return Base.HasLength()
+    Base.SizeUnknown()
+end
+
+function Base.IteratorSize(::Type{SortedIteratorProduct{T, F}}) where {T, F}
+    mapreduce(Base.IteratorSize, multiply, fieldtypes(T))
+end
+Base.IteratorSize(::Type{SortedIteratorProduct{Tuple{}, F}}) where F = Base.HasLength()
+Base.length(sip::SortedIteratorProduct) = prod(length, sip.sources, init=1)
+
+Base.IteratorEltype(::Type{SortedIteratorProduct}) = Base.HasEltype()
+Base.eltype(::Type{SortedIteratorProduct{T, F}}) where {T, F} = Tuple{eltype.(fieldtypes(T))...}
+
+
+
+### TODO move to CachedIterators.jl or somesuch
+struct Cached{T, C <: AbstractVector{T}, S}
+    cache::C
+    source::S
+end
+cached(iterator) = Cached(eltype(iterator)[], Base.Iterators.Stateful(iterator))
+cached(x::AbstractArray) = x
+
+Base.eltype(::Type{Cached{T}}) where T = T
+Base.firstindex(x::Cached) = firstindex(x.cache)
+
+function Base.checkbounds(::Type{Bool}, x::Cached, i::Integer)
+    i < firstindex(x.cache) && return false
+    while i > lastindex(x.cache)
+        isempty(x.source) && return false
+        push!(x.cache, popfirst!(x.source))
+    end
+    true
+end
+
+function Base.getindex(x::Cached, i::Integer)
+    checkbounds(Bool, x, i) # @inbounds not allowed
+    x.cache[i]
+end
+
+Base.iterate(x::Cached, i::Integer=firstindex(x)) = checkbounds(Bool, x, i) ? (x[i], i + 1) : nothing
+Base.IteratorSize(::Type{Cached{T, C, S}}) where {T, C, S} = Base.IteratorSize(S)
+Base.length(x::Cached) = length(x.cache) + length(x.source)
+Base.eltype(::Type{<:Cached{T}}) where T = T
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,104 @@
 using SortedIteratorProducts
 using Test
+using Base.Iterators
+using StatsBase
 
 @testset "SortedIteratorProducts.jl" begin
-    # Write your tests here.
+    @testset "IteratorSize" begin
+        @test Base.IteratorSize(SortedIteratorProduct(sum)) == Base.HasLength()
+        @test Base.IteratorSize(SortedIteratorProduct(sum, cycle(1:4))) == Base.IsInfinite()
+        @test Base.IteratorSize(SortedIteratorProduct(sum, 1:4, 1:7)) == Base.HasLength()
+        @test Base.IteratorSize(SortedIteratorProduct(sum, cycle(1:4), 1:7)) != Base.HasLength()
+        @test Base.IteratorSize(SortedIteratorProduct(sum, cycle(1:4), 1:0)) != Base.IsInfinite()
+    end
+
+    @testset "eltype" begin
+        @test eltype(SortedIteratorProduct(sum)) == Tuple{}
+        @test eltype(SortedIteratorProduct(sum, cycle(1:4))) == Tuple{Int}
+        @test eltype(SortedIteratorProduct(sum, 1:4, 1:7)) == Tuple{Int, Int}
+        @test eltype(SortedIteratorProduct(sum, cycle(1:4), 1:7)) == Tuple{Int, Int}
+        @test eltype(SortedIteratorProduct(sum, cycle(1:4), 1:0)) == Tuple{Int, Int}
+    end
+
+    @testset "length" begin
+        @test length(SortedIteratorProduct(sum)) == 1
+        @test_throws MethodError length(SortedIteratorProduct(sum, cycle(1:4)))
+        @test length(SortedIteratorProduct(sum, 1:4, 1:7)) == 4*7
+        @test_throws MethodError length(SortedIteratorProduct(sum, cycle(1:4), 1:7))
+        @test_broken length(SortedIteratorProduct(sum, cycle(1:4), 1:0)) == 0
+    end
+
+    function validate(x, n, by, iters...)
+        y = collect(product(iters...))
+        z = setdiff(y, x)
+
+        @test issorted(x; by=sum)
+        @test length(x) == n
+        @test x ⊆ y
+        @test allunique(x) || !allunique(y)
+
+        cmy = countmap(y)
+        cmx = countmap(x)
+        @test all(e -> cmx[e] ≤ cmy[e], x) # TODO use this test
+
+        @test all(by(a) ≤ by(b) for a in x for b in z)
+    end
+
+    @testset "end to end" begin
+        x = collect(take(SortedIteratorProduct(sum, 1:4, 1:7), 10)) # is this block helpful?
+        validate(x, 10, sum, 1:4, 1:7)
+        y = collect(take(SortedIteratorProduct(sum, 1:4, 1:7), 30))
+        validate(y, 28, sum, 1:4, 1:7)
+        z = collect(SortedIteratorProduct(sum, 1:4, 1:7))
+        validate(z, 28, sum, 1:4, 1:7)
+
+        x = collect(take(SortedIteratorProduct(sum, 1:4, 1:3:21), 10))
+        validate(x, 10, sum, 1:4, 1:3:21)
+        y = collect(take(SortedIteratorProduct(sum, 1:4, 1:3:21), 30))
+        validate(y, 28, sum, 1:4, 1:3:21)
+        z = collect(SortedIteratorProduct(sum, 1:4, 1:3:21))
+        validate(z, 28, sum, 1:4, 1:3:21)
+
+        @test y == z # stable! (TODO more thorough tests)
+
+        for i in SortedIteratorProduct(sum, 1:4, 1:3:22)
+            nothing
+        end
+    end
+
+    @testset "inf times zero" begin
+        x = SortedIteratorProduct(sum, repeated(8), 1:0)
+        @test iterate(x) === nothing
+        @test_broken length(x) == 0
+        @test_broken isempty(collect(x))
+    end
+
+    @testset "duplicate elements" begin
+        x = collect(take(SortedIteratorProduct(sum, [1,3,3,3,5], 1:3:1000), 10))
+        validate(x, 10, sum, [1,3,3,3,5], 1:3:1000)
+        y = collect(take(SortedIteratorProduct(sum, [1,3,3,3,5], 1:2:1000), 1729))
+        validate(y, 1729, sum, [1,3,3,3,5], 1:2:1000)
+    end
+end
+
+#= TODO test
+iteration on infinite
+unknown size
+fancy orders
+what happens when by is not nondecreasing is not satisfied
+performance
+=#
+
+
+
+### TODO move to CachedIterators.jl or somesuch
+@testset "CachedIterators.jl" begin
+    y = SortedIteratorProducts.cached(x^2 for x in 1:typemax(Int))
+    @test y[10] == 100
+    @test y[1] == 1
+    @test y[5] == 25
+    @test y[11] == 121
+    @test_throws BoundsError y[0]
+    @test_throws BoundsError y[-4]
+    @test_throws MethodError y[2.5]
 end
